6afeccab394eaf3761eb9ec212f0f587fb588a2e
tiandaye <q408596288@vip.qq.com>
2018-05-30 16:26:28 +0800

    first commit

---
+++ src/HelloComposer.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @Author: tianwangchong
+ * @Date:   2018-05-30 16:07:48
+ * @Last Modified by:   tianwangchong
+ * @Last Modified time: 2018-05-30 16:08:24
+ */
+namespace TianWangChong\HelloWorld;
+
+class HelloComposer
+{
+    public static function greet()
+    {
+        echo "Hello, Composer!", PHP_EOL;
+    }
+}
